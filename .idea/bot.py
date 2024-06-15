c267372afa81db696d62212abebb7442f2ddc467
ekaterina_pronina <ekat.mi3665@mail.ru>
2024-06-15 16:25:53 +0300

    Обработка нажатий

--- .idea/bot.py
+++ .idea/bot.py
@@ -7,11 +7,20 @@ logo = 'http://npcirs.ru/images/logo.png'
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    markup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton("Создание заявки")
-    btn2 = types.KeyboardButton("Другой вопрос")
-    btn3 = types.KeyboardButton("Контактная информация")
-    markup.add(btn1, btn2, btn3)
-    bot.send_message(message.chat.id, text="НПЦ ИРС \n чем я могу Вам помочь?", reply_markup=markup)
+    keyboard = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("Создание заявки", callback_data="create_request")
+    btn2 = types.InlineKeyboardButton("Другой вопрос", callback_data="other_question")
+    btn3 = types.InlineKeyboardButton("Контактная информация", callback_data="contact_info")
+    keyboard.add(btn1, btn2, btn3)
 
+    bot.send_message(message.chat.id, text="НПЦ ИРС \n чем я могу Вам помочь?", reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def handle_callback(call):
+    if call.data == "create_request":
+        bot.send_message(call.message.chat.id, "Вы нажали на кнопку 'Создание заявки'")
+    elif call.data == "other_question":
+        bot.send_message(call.message.chat.id, "Вы нажали на кнопку 'Другой вопрос'")
+    elif call.data == "contact_info":
+        bot.send_message(call.message.chat.id, "Вы нажали на кнопку 'Контактная информация'")
 bot.polling(none_stop=True, interval=0)
