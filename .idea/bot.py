8f700a2a300323cc0dbc17495088c8e1c9adf1eb
ekaterina_pronina <ekat.mi3665@mail.ru>
2024-06-16 14:52:52 +0300

    Информация от пользователя

--- .idea/bot.py
+++ .idea/bot.py
@@ -5,9 +5,10 @@ bot = telebot.TeleBot('6337501489:AAHF7J0K1435ZBHr8IohegDWsIZ0S6DB3Hc')
 
 logo = 'http://npcirs.ru/images/logo.png'
 
+user_data = {}
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_photo(chat_id=message.chat.id, photo=logo)
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     btn1 = types.InlineKeyboardButton("Создание заявки", callback_data="create_request")
     btn2 = types.InlineKeyboardButton("Другой вопрос", callback_data="other_question")
@@ -19,13 +20,42 @@ def send_welcome(message):
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback(call):
     if call.data == "create_request":
-        bot.send_message(call.message.chat.id, "Вы нажали на кнопку 'Создание заявки'")
+        bot.send_message(call.message.chat.id, "Пожалуйста, напишите Ваше ФИО")
+        bot.register_next_step_handler(call.message, get_surname)
     elif call.data == "other_question":
         bot.send_message(call.message.chat.id, "Пожалуйста, задайте Ваш вопрос")
     elif call.data == "contact_info":
         bot.send_message(call.message.chat.id, f"Тел.: (495)330-07-88\n"
                                                f"Факс: (495)330-56-01\n"
                                                f"Телефон горячей линии: 8-800-100-40-90\n"
-                                               f"E-mail: npcirs '@npcirs.ru'\n"
+                                               f"E-mail: npcirs@npcirs.ru\n"
                                                f"Адрес главного офиса: 117393, г. Москва, ул. Профсоюзная, д. 78, стр. 1; этаж 8\n")
+def get_surname(message):
+    user_data['surname'] = message.text
+    bot.send_message(message.chat.id, "Укажите ВК")
+    bot.register_next_step_handler(message, get_vk)
+def get_vk(message):
+    user_data['vk'] = message.text
+    bot.send_message(message.chat.id, "Укажите ЗАВ№")
+    bot.register_next_step_handler(message, get_numzav)
+def get_numzav(message):
+    user_data['numzav'] = message.text
+    bot.send_message(message.chat.id, "Опишите Вашу проблему")
+    bot.register_next_step_handler(message, get_description)
+def get_description(message):
+    user_data['description'] = message.text
+    bot.send_message(message.chat.id, "Отправьте фото проблемы")
+    bot.register_next_step_handler(message, get_photo)
+def get_photo(message):
+    user_data['photo'] = message.text
+    bot.send_message(message.chat.id, "Теперь укажите Ваш номер телефона")
+    bot.register_next_step_handler(message, get_phone)
+def get_phone(message):
+    user_data['email'] = message.text
+    bot.send_message(message.chat.id, "Укажите Вашу почту")
+    bot.register_next_step_handler(message, get_email)
+def get_email(message):
+    user_data['email'] = message.text
+    bot.send_message(message.chat.id, f"Спасибо! Ваши данные: \nФамилия: {user_data['surname']}\nТелефон: {user_data['phone']}")
+
 bot.polling(none_stop=True, interval=0)
